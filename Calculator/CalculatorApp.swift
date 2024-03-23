7d627efdbbfc2e4bdf3756e76d5dad5c0137158e
Eye-Mans <laluiman468@gmail.com>
2024-03-24 02:07:16 +0900

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Lalu Iman Abdullah on 24/03/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
